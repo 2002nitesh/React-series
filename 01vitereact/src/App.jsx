6d2df655e1afb5838b61aac9502e81afbbd69f0f
2002nitesh <kumarkumawatnitesh886@gmail.com>
2023-12-29 14:40:26 +0530

    .

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    // <></>   ->>>>>  fragment 
+    <>
+    <Chai />
+    <h1>chai aur react</h1>
+    <p>test pera</p>
+    </>
+  )
+}
+
+export default App
