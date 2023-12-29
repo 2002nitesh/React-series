a7911bb0d3ef4b6370094e9925b0517ae57ddefe
2002nitesh <kumarkumawatnitesh886@gmail.com>
2023-12-29 16:36:09 +0530

    .

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "nitesh";
 
   return (
     // <></>   ->>>>>  fragment 
     <>
     <Chai />
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test pera</p>
     </>
   )
